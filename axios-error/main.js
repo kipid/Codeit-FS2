0f4295c8c56541a8b440cb14e3b421159d92ce86
kipid <kipacti@google.com>
2024-08-22 17:26:10 +0900

    axios

---
+++ axios-error/main.js
@@ -0,0 +1,18 @@
+import { createAvatar } from './api.js';
+
+try {
+  const data = await createAvatar({
+    hairType: 'long5',
+    hairColor: 'black',
+    skin: 'tone100',
+    clothes: 'hoodie',
+    accessories: 'none',
+  });
+  console.log(data);
+} catch (e) {
+  console.log(e.message);
+  if (e.response) {
+    console.log(e.response.status);
+    console.log(e.response.data);
+  }
+}
