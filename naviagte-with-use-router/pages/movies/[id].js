6ad3c9b6b55dced847f1822bc80144fa35eb568d
kipid <kipacti@google.com>
2024-10-18 16:54:39 +0900

    codeitmall and Del navigate-with-use-router

--- naviagte-with-use-router/pages/movies/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function Movie() {
-  const router = useRouter();
-  const id = router.query['id'];
-
-  return <>Movie 페이지 #{id}</>;
-}
--- naviagte-with-use-router/pages/settings.js
+++
@@ -1,3 +0,0 @@
-export default function Settings() {
-  return <>Settings 페이지</>;
-}
